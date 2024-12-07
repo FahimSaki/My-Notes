1c2515fa691d1b3874932577055cd68d4e177a3e
FahimSaki <fahimuzzamaninferno@gmail.com>
2024-12-08 00:28:09 +0600

    Crud operations complete

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -37,6 +37,9 @@ class _NotesPageState extends State<NotesPage> {
               // add to DB
               context.read<NoteDatabase>().addNote(textController.text);
 
+              // clear controller
+              textController.clear();
+
               // pop dialog box
               Navigator.pop(context);
             },
@@ -53,8 +56,43 @@ class _NotesPageState extends State<NotesPage> {
   }
 
   // update a note
+  void updateNote(Note note) {
+    // pre-fill the current note text
+    textController.text = note.text;
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Update Note'),
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          // update button
+          MaterialButton(
+            onPressed: () {
+              // update note in DB
+              context
+                  .read<NoteDatabase>()
+                  .updateNotes(note.id, textController.text);
+
+              // clear controller
+              textController.clear();
+
+              // pop dialog box
+              Navigator.pop(context);
+            },
+            child: const Text('Update'),
+          ),
+        ],
+      ),
+    );
+  }
 
   // delete a note
+  void deleteNote(int id) {
+    context.read<NoteDatabase>().deleteNote(id);
+  }
+
   @override
   Widget build(BuildContext context) {
     // note database
@@ -80,6 +118,22 @@ class _NotesPageState extends State<NotesPage> {
           // list tile UI
           return ListTile(
             title: Text(note.text),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // edit button
+                IconButton(
+                  onPressed: () => updateNote(note),
+                  icon: const Icon(Icons.edit),
+                ),
+
+                // delete button
+                IconButton(
+                  onPressed: () => deleteNote(note.id),
+                  icon: const Icon(Icons.delete),
+                ),
+              ],
+            ),
           );
         },
       ),
