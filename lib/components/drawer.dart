0e3b0a2c10e5905d143aeb9f73fb1c75eb1cf658
FahimSaki <fahimuzzamaninferno@gmail.com>
2024-12-15 21:45:01 +0600

    Drawer Done

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notes/components/drawer_tile.dart';
+import 'package:notes/pages/settings_page.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
@@ -31,8 +33,26 @@ class MyDrawer extends StatelessWidget {
           ),
 
           // notes tile
+          DrawerTile(
+            title: 'Notes',
+            leading: const Icon(Icons.home),
+            onTap: () => Navigator.pop(context),
+          ),
 
           // settings tile
+          DrawerTile(
+            title: 'Notes',
+            leading: const Icon(Icons.settings),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SettingsPage(),
+                ),
+              );
+            },
+          ),
         ],
       ),
     );
